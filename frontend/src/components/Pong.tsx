cdaff00b37c5d54a6d8bb7eaafe8634e4a54465f
lboiteux <loevanbtx.pro@gmail.com>
2025-05-29 17:00:42 +0200

    update pong.tsx

--- frontend/src/components/Pong.tsx
+++ frontend/src/components/Pong.tsx
@@ -1,16 +1,5 @@
 import React, { useEffect, useRef } from 'react';
 
-const Pong: React.FC = () => {
-  const canvasRef = useRef<HTMLCanvasElement | null>(null);
-
-  useEffect(() => {
-    const ctx = canvasRef.current?.getContext('2d');
-    if (ctx) {
-      ctx.fillText('Pong Game Here', 350, 200);
-    }
-  }, []);
-
-  return <canvas ref={canvasRef} className="border" width={800} height={400}></canvas>;
-};
+const Pong: React.FC = () => {};
 
 export default Pong;
